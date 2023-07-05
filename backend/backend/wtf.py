6ba5fcb39b41e5ca9f9d912dff54bbc6de79cddf
4erdenko <4erdenko@tuta.io>
2023-07-05 12:47:08 +0300

    Make Nechai Great Again!

--- backend/backend/wtf.py
+++ backend/backend/wtf.py
@@ -5,11 +5,9 @@ from cryptography.fernet import Fernet
 
 class DecryptView(APIView):
     def get(self, request, format=None):
-        # Ваш зашифрованный текст
         cipher_text = b"gAAAAABkpTu4jSiVxR3tST1QM7VWtK0Ld-ZeXymBUaQMQx271d95V8D3ReRFlStH_0f8nUhJsQXmxIl8IotIvzIGBKGJuzd0Hbk-qN9pi8-nzS-AdA2dVDE="
 
-        # Используйте ваш ключ для дешифрования
-        key = b"Teg2f5LYVSWTS-E77Gnltiy287qCFBqsWc68m0Is_Dg="  # Ключ должен храниться в безопасном месте, не в коде!
+        key = b"Teg2f5LYVSWTS-E77Gnltiy287qCFBqsWc68m0Is_Dg="
         cipher_suite = Fernet(key)
         plain_text = cipher_suite.decrypt(cipher_text)
 
