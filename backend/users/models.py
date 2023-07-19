2be42a5bdd2ffc491a8efa521738b568cabf25ce
4erdenko <4erdenko@tuta.io>
2023-07-20 00:41:31 +0300

    review[5]

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,7 @@
-from backend import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+from backend import settings
 from users.validators import validate_username
 
 
