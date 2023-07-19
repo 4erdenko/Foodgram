684a4c099accd06243f273bb6580cff41ff4eabb
4erdenko <4erdenko@tuta.io>
2023-07-20 00:25:55 +0300

    isort

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,8 @@
-from backend import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from users.validators import validate_username
+
+from backend import settings
 
 
 class User(AbstractUser):
