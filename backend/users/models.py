a0af2027f0f5db94b6b04b504315e16c9929d7c0
4erdenko <4erdenko@tuta.io>
2023-07-21 15:03:56 +0300

    from django.conf import settings

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from users.validators import validate_username
 
-from backend import settings
+from django.conf import settings
 
 
 class User(AbstractUser):
